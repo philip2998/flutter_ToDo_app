eeef091074ad618017d395fc320537103049948b
Philip Gevorgyan <filipgevorgyan@gmail.com>
2023-11-03 13:33:29 +0400

    Created Todo item section, implemented functionality for todo list

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -3,40 +3,60 @@ import 'package:flutter/material.dart';
 import '../constants/colors.dart';
 import '../model/todo.dart';
 
-class TodoItem extends StatelessWidget {
-  final ToDo toDo;
+class ToDoItem extends StatelessWidget {
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
 
-  const TodoItem({super.key, required this.toDo});
+  const ToDoItem({
+    Key? key,
+    required this.todo,
+    required this.onToDoChanged,
+    required this.onDeleteItem,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(bottom: 20),
       child: ListTile(
-        onTap: () {},
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        onTap: () {
+          onToDoChanged(todo);
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
         contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(
-            toDo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
-            color: tdBlue),
-        title: Text(toDo.todoText!,
-            style: TextStyle(
-                fontSize: 16,
-                color: tdBlack,
-                decoration: toDo.isDone ? TextDecoration.lineThrough : null)),
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdBlue,
+        ),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: todo.isDone ? TextDecoration.lineThrough : null,
+          ),
+        ),
         trailing: Container(
           padding: EdgeInsets.all(0),
           margin: EdgeInsets.symmetric(vertical: 12),
           height: 35,
           width: 35,
           decoration: BoxDecoration(
-              color: tdRed, borderRadius: BorderRadius.circular(5)),
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
           child: IconButton(
-            icon: Icon(Icons.delete),
             color: Colors.white,
             iconSize: 18,
-            onPressed: () {},
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              // print('Clicked on delete icon');
+              onDeleteItem(todo.id);
+            },
           ),
         ),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:small_flutter_challenge/widgets/main.dart';
+import 'package:small_flutter_challenge/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
