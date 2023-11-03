042c2c1e42cb44c13f8020dd3793d9e7967b4f86
Philip Gevorgyan <filipgevorgyan@gmail.com>
2023-11-03 11:11:51 +0400

    initial commit

---
+++ android/app/src/main/kotlin/com/example/small_flutter_challenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.small_flutter_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('ToDo App'),
+        ),
+        body: Container(
+          child: Text('This is home screen'),
+        ));
+  }
+}
